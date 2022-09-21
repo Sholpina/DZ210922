fb620c1c5493d1e72de3ef112bfb03e92e1a17a4
Sholpan <30983083+Sholpina@users.noreply.github.com>
2022-09-21 14:05:40 +0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,35 +4,35 @@ import json
 import openpyxl
 
 
+book = openpyxl.Workbook()
+sheet = book.active
+
 response = requests.get(url="https://jsonplaceholder.typicode.com/todos")
 data = response.json()
 print(data)
 
  # запись объектов в файл json
 for k, value in enumerate(data):
-        with open(f"files/data.json{k}", 'w', encoding="utf-8") as f:
+        filename = f'files/data.json{k}'
+        with open(filename, 'w', encoding="utf-8") as f:
                 json.dump(data[k], f, ensure_ascii=False, indent=8)
 
 # прочитать все файлы из папки и сохранить в массив
 data_from_folders = []
 for i in range(0, len(data)):
         filename = f'files/data.json{i}'
-        data_dict= open(filename)    
-        dt= json.load(data_dict)      #deserialization
+        data_dict= open(filename)    #deserialization
+        dt= json.load(data_dict)
         data_from_folders.append(dt)
         print((f'Saved file {filename}'))
 print("Datatype after de-serialisation: " + str(type(dt)))
 
 
-exel_file = openpyxl.Workbook()
-sheet = exel_file.active
 
 sheet['A1'] = "userId"
 sheet['B1'] = "id"
 sheet['C1'] ="title"
 sheet['D1'] ="completed"
-
-
 row = 2
 for i in range(0, len(data)):
         filename = f'files/data.json{i}'
@@ -45,7 +45,7 @@ for i in range(0, len(data)):
                 sheet[row][3] .value= dt["completed"]
                 row +=1
         print(f'Saved file {filename}')
-print("Total saved files: " ,  len(data))
 
-exel_file.save("mybook.xlsx")
-exel_file.close()
+book.save("my_book.xlsx")
+book.close()
+print("Total saved files: " ,  len(data))
